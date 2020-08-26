4b239166aff04eaabeb64f353caf2e2062d1fdec
kjx63 <ueyama.kenji.63@gmail.com>
2020-08-26 14:28:18 +0900

    Build SwipeButtons Components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import TinderCards from './components/tindercards/TinderCards';
+import SwipeButtons from './components/swipebuttons/SwipeButtons';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           </Route>
           <Route path='/'>
             <TinderCards />
+            <SwipeButtons />
           </Route>
         </Switch>
       </Router>
