ff1d02eb23d7211f19ba956ef34aa00cdf0b85cb
kjx63 <ueyama.kenji.63@gmail.com>
2020-08-26 10:27:51 +0900

    Create Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className='app'>
-      <h1>Let's Build a Tinder Clone 🚀🚀! </h1>
+      <Header />
     </div>
   );
 }
